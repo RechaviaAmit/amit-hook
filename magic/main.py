e16d454d85588007da9776b3764653aa050c3d99
Amit Rechavia <amit.rechavia@intel.com>
2024-07-10 23:28:48 +0300

    add entry_point

--- magic/main.py
+++ magic/main.py
@@ -1,5 +1,4 @@
 # The hook must exit nonzero on failure or modify files.
-# My choice is to edit the file
 
 import argparse
 import sys
@@ -14,11 +13,11 @@ def main(argv=None):
             contents = f.read()
         if "hi" in contents:
             print("stop being so polite!")
-            return 1
+            sys.exit(1)
         with open(filename, 'w') as f:
             f.write(contents.replace('thanks', 'thanks May & Zohar for organizing this amazing meeting!'))
-    return 0
+    sys.exit(0)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    main()
--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='amit_hook',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'magic = magic.main:main',
+        ],
+    },
 )
